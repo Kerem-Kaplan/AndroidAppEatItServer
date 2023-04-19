ea2a030b1b758237cd71c549694cb832c0afddf0
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-04-20 00:53:02 +0300

    server side delete and update functions

--- app/src/main/java/dev/krm/androidappeatitserver/Home.java
+++ app/src/main/java/dev/krm/androidappeatitserver/Home.java
@@ -44,6 +44,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.UUID;
 
 import dev.krm.androidappeatitserver.Common.Common;
+import dev.krm.androidappeatitserver.Interface.ItemClickListener;
 import dev.krm.androidappeatitserver.Model.Category;
 import dev.krm.androidappeatitserver.ViewHolder.MenuViewHolder;
 import dev.krm.androidappeatitserver.databinding.ActivityHomeBinding;
@@ -226,6 +227,12 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 menuViewHolder.txtMenuName.setText(category.getName());
                 Picasso.with(Home.this).load(category.getImage()).into(menuViewHolder.imageView);
 
+                menuViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                    }
+                });
 
             }
         };
@@ -257,4 +264,110 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        if (item.getTitle().equals(Common.UPDATE)) {
+            showUpdateDialog(adapter.getRef(item.getOrder()).getKey(), adapter.getItem(item.getOrder()));
+        } else if (item.getTitle().equals(Common.DELETE)) {
+            deleteCategory(adapter.getRef(item.getOrder()).getKey());
+        }
+
+
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteCategory(String key) {
+        categories.child(key).removeValue();
+        Toast.makeText(this, "Item deleted!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showUpdateDialog(String key, Category item) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Home.this);
+        alertDialog.setTitle("Update Category");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_menu_layout = inflater.inflate(R.layout.add_new_menu_layout, null);
+
+        edtName = add_menu_layout.findViewById(R.id.edtName);
+        btnSelect = add_menu_layout.findViewById(R.id.btnSelect);
+        btnUpload = add_menu_layout.findViewById(R.id.btnUpload);
+
+        edtName.setText(item.getName());
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImage(item);
+            }
+        });
+
+        alertDialog.setView(add_menu_layout);
+        alertDialog.setIcon(R.drawable.baseline_shopping_cart_24);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+                item.setName(edtName.getText().toString());
+                categories.child(key).setValue(item);
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+
+    }
+
+    private void changeImage(Category category) {
+        if (saveUri != null) {
+            ProgressDialog mDialog = new ProgressDialog(this);
+            mDialog.setMessage("Uploading...");
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            StorageReference imageFolder = storageReference.child("images/" + imageName);
+            imageFolder.putFile(saveUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mDialog.dismiss();
+                    Toast.makeText(Home.this, "Uploaded!", Toast.LENGTH_SHORT).show();
+                    imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            category.setImage(uri.toString());
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    mDialog.dismiss();
+                    Toast.makeText(Home.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                    double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                    mDialog.setMessage("Uploaded" + progress + "%");
+                }
+            });
+        }
+    }
+
 }
+
--- app/src/main/java/dev/krm/androidappeatitserver/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/dev/krm/androidappeatitserver/ViewHolder/MenuViewHolder.java
@@ -1,5 +1,6 @@
 package dev.krm.androidappeatitserver.ViewHolder;
 
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,10 +8,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import dev.krm.androidappeatitserver.Common.Common;
 import dev.krm.androidappeatitserver.Interface.ItemClickListener;
 import dev.krm.androidappeatitserver.R;
 
-public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+        , View.OnCreateContextMenuListener {
     public TextView txtMenuName;
     public ImageView imageView;
 
@@ -18,18 +21,27 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
 
     public MenuViewHolder(@NonNull View itemView) {
         super(itemView);
-        txtMenuName=(TextView) itemView.findViewById(R.id.menu_name);
-        imageView=(ImageView) itemView.findViewById(R.id.menu_image);
+        txtMenuName = (TextView) itemView.findViewById(R.id.menu_name);
+        imageView = (ImageView) itemView.findViewById(R.id.menu_image);
+        itemView.setOnCreateContextMenuListener(this);
 
         itemView.setOnClickListener(this);
     }
 
-    public void setItemClickListener(ItemClickListener itemClickListener){
-        this.itemClickListener=itemClickListener;
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
     }
 
     @Override
     public void onClick(View view) {
-        itemClickListener.onClick(view,getAdapterPosition(),false);
+        itemClickListener.onClick(view, getAdapterPosition(), false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Select the action");
+
+        menu.add(0,0,getAdapterPosition(), Common.UPDATE);
+        menu.add(0,1,getAdapterPosition(), Common.DELETE);
     }
 }
