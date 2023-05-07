40ad7b7f79764d3fc67a73e5a7dd355a9c83395f
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-05-07 16:29:09 +0300

    added google maps

--- app/src/main/java/dev/krm/androidappeatitserver/Common/Common.java
+++ app/src/main/java/dev/krm/androidappeatitserver/Common/Common.java
@@ -1,7 +1,14 @@
 package dev.krm.androidappeatitserver.Common;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+
 import dev.krm.androidappeatitserver.Model.Request;
 import dev.krm.androidappeatitserver.Model.User;
+import dev.krm.androidappeatitserver.Remote.IGeoCoordinates;
+import dev.krm.androidappeatitserver.Remote.RetrofitClient;
 
 public class Common {
     public static User currentUser;
@@ -11,6 +18,8 @@ public class Common {
     public  static final String DELETE="Delete";
     public static final int PICK_IMAGE_REQUEST = 71;
 
+    public static final String baseUrl="https://maps.googleapis.com";
+
     public static String convertCodeToStatus(String code){
         if(code.equals("0"))
             return "Placed";
@@ -18,5 +27,26 @@ public class Common {
             return "On my way";
         else
             return "Shipped";
+    }
+
+    public static IGeoCoordinates getGeoCooordinates(){
+        return RetrofitClient.getClient(baseUrl).create(IGeoCoordinates.class);
+    }
+
+    public static Bitmap scaleBitmap(Bitmap bitmap,int newWidth,int newHeight){
+        Bitmap scaledBitmap=Bitmap.createBitmap(newWidth,newHeight,Bitmap.Config.ARGB_8888);
+
+        float scaleX=newWidth/(float)bitmap.getWidth();
+        float scaleY=newHeight/(float)bitmap.getHeight();
+        float pivotX=0,pivotY=0;
+
+        Matrix scaleMatrix=new Matrix();
+        scaleMatrix.setScale(scaleX,scaleY,pivotX,pivotY);
+
+        Canvas canvas=new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bitmap,0,0,new Paint(Paint.FILTER_BITMAP_FLAG));
+
+        return scaledBitmap;
     }
 }
--- app/src/main/java/dev/krm/androidappeatitserver/TrackingOrder.java
+++ app/src/main/java/dev/krm/androidappeatitserver/TrackingOrder.java
@@ -1,31 +1,44 @@
 package dev.krm.androidappeatitserver;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationRequest;
-import android.os.Bundle;
-import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.PrimitiveIterator;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.List;
+
+import dev.krm.androidappeatitserver.Common.Common;
+import dev.krm.androidappeatitserver.Remote.IGeoCoordinates;
 import dev.krm.androidappeatitserver.databinding.ActivityTrackingOrderBinding;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TrackingOrder extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -34,17 +47,19 @@ public class TrackingOrder extends FragmentActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
 
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST=1000;
-    private final static int LOCATION_PERMISSION_REQUEST=1001;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private final static int LOCATION_PERMISSION_REQUEST = 1001;
 
     private Location mLastLocation;
 
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
 
-    private static int UPDATE_INTERVAL=1000;
-    private static int FATEST_INTERVAL=5000;
-    private static int DISPLACEMENT=10;
+    private static int UPDATE_INTERVAL = 1000;
+    private static int FATEST_INTERVAL = 5000;
+    private static int DISPLACEMENT = 10;
+
+    private IGeoCoordinates mService;
 
     private ActivityTrackingOrderBinding binding;
 
@@ -55,32 +70,113 @@ public class TrackingOrder extends FragmentActivity implements OnMapReadyCallbac
         binding = ActivityTrackingOrderBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
-                &&ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED )
-        {
+        mService= Common.getGeoCooordinates();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestRuntimePermission();
-        }else{
-            if(checkPlayServices()){
+        } else {
+            if (checkPlayServices()) {
                 buildGoogleApiClient();
                 createLocationRequest();
             }
         }
+
+        displayLocation();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    private void buildGoogleApiClient() {
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestRuntimePermission();
+        } else {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                double latitude = mLastLocation.getLatitude();
+                double longitude = mLastLocation.getLongitude();
+
+                LatLng yourLocation = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(yourLocation).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(yourLocation));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+
+                drawRoute(yourLocation,Common.currentRequest.getAddress());
+
+            } else {
+                Toast.makeText(this, "Couldn't get the location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void drawRoute(LatLng yourLocation, String address) {
+        mService.getGeoCode(address).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                try {
+                    JSONObject jsonObject=new JSONObject(response.body().toString());
+
+                    String lat=((JSONArray)jsonObject.get("results"))
+                            .getJSONObject(0)
+                            .getJSONObject("geometry")
+                            .getJSONObject("location")
+                            .get("lat").toString();
+
+                    String lng=((JSONArray)jsonObject.get("results"))
+                            .getJSONObject(0)
+                            .getJSONObject("geometry")
+                            .getJSONObject("location")
+                            .get("lng").toString();
+
+                    LatLng orderLocation=new LatLng(Double.parseDouble(lat),Double.parseDouble(lng));
+
+                    Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.box);
+                    bitmap=Common.scaleBitmap(bitmap,70,70);
+
+                    MarkerOptions marker=new MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(bitmap))
+                            .title("Order of "+Common.currentRequest.getPhone())
+                            .position(orderLocation);
+                    mMap.addMarker(marker);
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        mGoogleApiClient.connect();
     }
 
     private boolean checkPlayServices() {
-        int resultCode= GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if(resultCode!=ConnectionResult.SUCCESS){
-            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)){
-                GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            }
-            else{
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
                 Toast.makeText(this, "This device is not support", Toast.LENGTH_SHORT).show();
                 finish();
             }
@@ -90,56 +186,75 @@ public class TrackingOrder extends FragmentActivity implements OnMapReadyCallbac
     }
 
     private void requestRuntimePermission() {
-        ActivityCompat.requestPermissions(this,new String[]{
+        ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
-        },LOCATION_PERMISSION_REQUEST);
+        }, LOCATION_PERMISSION_REQUEST);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
+        switch (requestCode) {
             case LOCATION_PERMISSION_REQUEST:
-                    if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                        if(checkPlayServices()){
-                            buildGoogleApiClient();
-                            createLocationRequest();
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayServices()) {
+                        buildGoogleApiClient();
+                        createLocationRequest();
 
-                            displayLocation();
-                        }
+                        displayLocation();
                     }
-                    break;
+                }
+                break;
         }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-
-    }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        displayLocation();
+        startLocationUpdates();
+    }
 
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (com.google.android.gms.location.LocationListener) this);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        mLastLocation = location;
+        displayLocation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mGoogleApiClient!=null)
+            mGoogleApiClient.connect();
     }
 }
